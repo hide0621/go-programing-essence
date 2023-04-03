6eed206a199f2d69e4f2bbf0884a8a9b13b8c043
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-04-03 11:56:01 +0900

    12-2章 entを使ってCRUDのうちのcreateを書いてみた

---
+++ Chapter12/12-2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go-programing-essence/Chapter12/12-2/ent"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+
+	// スキーマ作成（マイグレーション）
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// Todoを列挙
+	for _, e := range client.Todo.Query().AllX(context.Background()) {
+		fmt.Println(e.Text)
+	}
+
+	// Todoを作成
+	_, err = client.Todo.Create().SetText("Go言語を学ぶ").Save(context.Background())
+	if err != nil {
+		log.Fatalf("failed creating a todo: %V", err)
+	}
+}
