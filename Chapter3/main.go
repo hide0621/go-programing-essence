ae8dead22dc72ea2344c374111993e02a9661ae8
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-04-11 15:35:36 +0900

    3-4章 labeled break法について学習した

--- Chapter3/main.go
+++ Chapter3/main.go
@@ -1,24 +1,8 @@
 package main
 
-import "fmt"
-
-// const (
-// 	Apple = iota
-// 	Orange
-// 	Banana
-// )
-
-// const (
-// 	Apple = iota + iota
-// 	Orange
-// 	Banana
-// )
-
-// const (
-// 	Apple = iota + iota
-// 	Orange
-// 	Banana = iota + 3
-// )
+import (
+	"fmt"
+)
 
 type Fruit int
 type Animal int
@@ -35,7 +19,37 @@ const (
 	Pig
 )
 
-func main() {
+func list_iota(a int) {
+	// const (
+	// 	Apple = iota
+	// 	Orange
+	// 	Banana
+	// )
+
+	// const (
+	// 	Apple = iota + iota
+	// 	Orange
+	// 	Banana
+	// )
+
+	// const (
+	// 	Apple = iota + iota
+	// 	Orange
+	// 	Banana = iota + 3
+	// )
+
+	const (
+		Apple Fruit = iota
+		Orange
+		Banana
+	)
+
+	const (
+		Monckey Animal = iota
+		Elephant
+		Pig
+	)
+
 	// fmt.Println(Apple)
 	// fmt.Println(Orange)
 	// fmt.Println(Banana)
@@ -45,4 +59,32 @@ func main() {
 
 	// fruit = Elephant
 	// fmt.Println(fruit)
+
+}
+
+func list_loop(b int) {
+
+outerLoop:
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Outer loop iteration %d\n", i)
+
+		for j := 0; j < 5; j++ {
+			fmt.Printf("  Inner loop iteration %d\n", j)
+
+			// Inner loopの3回目でouterLoopから脱出
+			if i == 2 && j == 2 {
+				break outerLoop
+			}
+		}
+	}
+
+	fmt.Println("Done")
+}
+
+func main() {
+
+	list_iota(1)
+
+	list_loop(1)
+
 }
